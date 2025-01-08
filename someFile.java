ce82b3591269d47d05c8928680b8c13d63cb83e8
SuleimanMusaev <suli141198@gmail.com>
2025-01-09 03:07:26 +0600

    java. Back-End (#10)
    * java
    
    * someFile

--- someFile.java
+++ someFile.java
@@ -1,8 +1,18 @@
+// comment 1
+// comment 2
+// comment 3
+// comment 4
+// solution
+// is correct?
+
+//from Yegor
 //comment 756
 //comment 1.5.2
 //comment 26
 //comment 32
 // solutions
+
+//from Kira
 //comment 1
 //comment 1.2
 //comment 2
